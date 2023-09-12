eef3499f307ae50b468ba9a1c2075aca19763ab8
Gang <ganglix@gmail.com>
2023-09-12 16:20:37 -0600

    week1

--- week_1/ch02_intro.py
+++ week_1/ch02_intro.py
@@ -1,21 +1,16 @@
 # atomic data : int, float, boolean (True or False)
 # compound data :
 # string,
-'apple'
-"apple"
-'"show quotation"'
-''
-' '
+
 # list
-a = [1, 2, 3, 'apple']
-print(a)
 
 # tuple
-a = (1, 2, 3)
 
-# dictionary, and many more (such as numpy arrays, pandas dataframe)
-a = {'passcode': "12345"}
-print(a)
+# dictionary, and many more
+
+
+
+
 
 # operators
 # arithmetic
