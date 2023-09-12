9038a2a2a507b3b34319bd273c1d7bba46008c4c
Gang <ganglix@gmail.com>
2023-09-12 16:04:15 -0600

    week1 prep-full

---
+++ week_1/ch02_intro.py
@@ -0,0 +1,36 @@
+# atomic data : int, float, boolean (True or False)
+# compound data :
+# string,
+'apple'
+"apple"
+'"show quotation"'
+''
+' '
+# list
+a = [1, 2, 3, 'apple']
+print(a)
+
+# tuple
+a = (1, 2, 3)
+
+# dictionary, and many more (such as numpy arrays, pandas dataframe)
+a = {'passcode': "12345"}
+print(a)
+
+# operators
+# arithmetic
+# +, -, /, *, //, %
+# a ** b
+
+
+# comparison
+# >, <, ==, !=
+
+# membership
+# in, not in
+
+
+
+# things I want to mention on Tue: re-assign variable (Ex4), I/O
+
+# show I/O in pycharm editor and console using Ex2
---
+++ week_1/ch03_intro.py
@@ -0,0 +1,10 @@
+# What Python function displays text on the console? (how about the return value?)
+# print("hello")
+# print( print("hello") )  #print() has no return value
+
+
+# What Python function retrieves user input from the
+# console (keyboard)?
+
+# number = input("type an integer: ")
+# print("the number you just typed in is: ", number)
