7f87c9c4cd5f3e8953cbc2c4291a71df1cf1dab7
Gang <ganglix@gmail.com>
2023-09-14 22:24:13 -0600

    add console history

--- week_1/ch03_demo2.py
+++ week_1/ch03_demo2.py
@@ -14,3 +14,32 @@ type
 """
 
 # console history showed in class
+# Last login: Thu Sep 14 17:12:27 on ttys000
+# (base) gangli@Gang-Lis-MacBook-Pro ~ % python
+# Python 3.11.3 (main, Apr 19 2023, 18:51:09) [Clang 14.0.6 ] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> len("cmpt 141")
+# 8
+# >>> max(1,2,3)
+# 3
+# >>> a = "apple"
+# >>> type(a)
+# <class 'str'>
+# >>> type(print)
+# <class 'builtin_function_or_method'>
+# >>> return_of_fun = print("cmpt")
+# cmpt
+# >>> print(return_of_fun)
+# None
+# >>> max('a', 'b')
+# 'b'
+# >>> max('a', 'b', 'z')
+# 'z'
+# >>> max('a', 'B')
+# 'a'
+# >>> max('A', 'b')
+# 'b'
+# >>> max('a',"A")
+# 'a'
+# >>> max('.','a')
+# 'a'
