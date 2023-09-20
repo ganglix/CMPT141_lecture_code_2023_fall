bde34fb5a18bf16f08f26e81a46441da37e46886
Gang Li <ganglix@gmail.com>
2023-09-19 18:50:56 -0600

    week1 in class

--- week_2/ch04_demo1.py
+++ week_2/ch04_demo1.py
@@ -2,15 +2,23 @@
 # returns the absolute value of a number):
 
 def print_smaller_absolute(num1, num2):
+    """This function takes two numbers and
+    return the absolute value of the smaller number and print this value
+    :param num1: int or float, first number
+    :param num2: int or float, second number
+    :return: int or float, the absolute value of the smaller number
+    """
     small_abs = abs(min(num1, num2))
     print("Absolute value of smaller number: ", small_abs)
     return small_abs
+
+help(print_smaller_absolute)
 
 
 # things not to do.
 # afraid to lose marks
 # example in an engineering software program?
-# style
+# style, numpy, google
 
 
 # help(max)
--- week_2/ch04_ex1_ex2.py
+++ week_2/ch04_ex1_ex2.py
@@ -5,3 +5,15 @@
 # (c) Example: if called with arguments 9 and 99, the function
 # should return the string $9.99
 
+def format_price(dollar, cent):
+    """ format price as "$D.C." with the amount of dollar and cent and return the formatted string
+    :param dollar: integer, dollar amount
+    :param cent: integer, cent amount
+    :return: str, formatted price
+    """
+    formatted = "$" + str(dollar) + "." + str(cent)
+    print(formatted)
+    return formatted
+
+
+print("print outside of the function:", format_price(9, 99))
