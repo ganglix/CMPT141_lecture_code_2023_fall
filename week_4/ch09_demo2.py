fbd38c2bf0badeba72a697235e0f398509458fb8
Gang Li <ganglix@gmail.com>
2023-10-05 14:25:02 -0600

    week4 prep Thur

--- week_4/ch09_demo2.py
+++ week_4/ch09_demo2.py
@@ -5,21 +5,11 @@
 # password example
 
 # initiate param
-passcode = "cmpt141"
-max_attempt = 3
-code = input("input the passcode: ")
-n_attempt = 1
 
 # ask user to input passcode
-while code != passcode and n_attempt <= (max_attempt-1):
-    code = input("input the passcode: ")
-    n_attempt += 1
+
 
 # print message to user
-if code == passcode:
-    print(f"passcode is correct. {n_attempt} attempt(s) made")
-else:
-    print(f"maximum {n_attempt}/{max_attempt} attempts reached, your are locked out")
 
 
 
