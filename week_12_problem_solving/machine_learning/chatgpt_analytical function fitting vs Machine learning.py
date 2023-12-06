ee47b5e0e083dc53a4714b06c2094a761f146dd7
Gang <ganglix@gmail.com>
2023-12-06 10:53:20 -0600

    prepare

--- week_12_problem_solving/machine_learning/chatgpt_analytical function fitting vs Machine learning.py
+++ week_12_problem_solving/machine_learning/chatgpt_analytical function fitting vs Machine learning.py
@@ -21,51 +21,75 @@ y_data = true_func(x_data) + np.random.normal(scale=0.1, size=100)
 plt.plot(x_data, y_data, '.')
 plt.show()
 
+
 # Define the function
-def guessed_func(x, a, b, c):
-    return a * np.sin(b * x) + c
+def guessed_func(x, a, b):
+    return a * x + b
 
 
 # Fit the data using curve_fit
-parameters_initial_guess = [1, 1, 1]
-popt, pcov = curve_fit(guessed_func, x_data, y_data, parameters_initial_guess)
 
-plt.plot(x_data, y_data, 'o', label='data')
-plt.plot(x_data, guessed_func(x_data, *popt), '-', label='function ift')
-plt.title( f"a * np.sin(b * x) + c parameters \n (a, b, c) = {popt}")
-plt.show()
-print(pcov)
+
+
+
+
+
+
+
+
+def calculate_r_squared(y_data, y_predicted_data):
+    """
+    chatgpt Prompt:
+    Write a function to calculate the R-squared for goodness of fit,
+    given two arrays: y_data and y_predicted_data
+    """
+    # Calculate the mean of the actual values (y_data)
+    mean_y_data = np.mean(y_data)
+
+    # Calculate the total sum of squares (TSS)
+    tss = np.sum((y_data - mean_y_data) ** 2)
+
+    # Calculate the residual sum of squares (RSS)
+    rss = np.sum((y_data - y_predicted_data) ** 2)
+
+    # Calculate R-squared (coefficient of determination)
+    r_squared = 1 - (rss / tss)
+
+    return r_squared
+
 
 # # machine learning to fit the data
-# """
-# ChatGPT prompt:
-# fit the data (similar to sin(x)) using an artificial neural network with scikit-learn module. use all data for fit/train the model, no test
-#
-# """
-#
-# from sklearn.neural_network import MLPRegressor
-#
-# def ml_fit(x_data, y_data):
-#     # Generate the data points
-#     x_data = x_data.reshape(-1, 1)
-#     y_data = y_data.reshape(-1, 1)
-#
-#     # Define the neural network model
-#     model = MLPRegressor(hidden_layer_sizes=(10, 10), activation='tanh', solver='lbfgs')
-#
-#     # Train the model on the data
-#     model.fit(x_data, y_data)
-#
-#     # Generate the fitted curve
-#     y_fit = model.predict(x_data)
-#
-#     return y_fit
-#
+"""
+ChatGPT prompt:
+fit the data (similar to sin(x)) using an artificial neural network with scikit-learn module. 
+use all data for fit/train the model, no test
+"""
+
+from sklearn.neural_network import MLPRegressor
+
+# Generate the data points
+x_data = x_data.reshape(-1, 1)
+y_data = y_data.reshape(-1, 1)
+
+# Define the neural network model
+model = MLPRegressor(hidden_layer_sizes=(5, 5), activation='tanh', solver='lbfgs')
+
+# Train the model on the data
+model.fit(x_data, y_data)
+
+# Generate the fitted curve
+y_fit = model.predict(x_data)
+
 
 # # Plot the data and the fitted curve
-# plt.plot(x_data, y_data, 'o', label='data')
+plt.plot(x_data, y_data, 'o', label='data')
 # plt.plot(x_data, function_fit(x_data, y_data, noisy_func), '-', label='function ift')
-# # plt.plot(x_data, ml_fit(x_data, y_data), '-', label='ML fit')
-#
-# plt.legend()
-# plt.show()
+plt.plot(x_data, y_fit, '-', label='ML fit')
+
+# print(calculate_r_squared(y_data, y_fit))
+plt.legend()
+plt.show()
+
+
+
+
