ad6b7119226e7851b0d18ef9d62bac42ed2906e0
Gang Li <ganglix@gmail.com>
2023-12-05 18:56:12 -0600

    in class

--- week_12_problem_solving/weather/weather_naiveplot.py
+++ week_12_problem_solving/weather/weather_naiveplot.py
@@ -36,42 +36,42 @@ def get_temperature_data():
     return temperature_data
 
 
-# def avg_monthly_temp(temperature_data, start_year,
-#                      end_year, temp_column_index):
-#     """
-#     Calculate the mean of the temperatures in column temp_column_index for the given range of years.
-#
-#     :param temperature_data: a list in which each item is a temperature data record as a list
-#     :param start_year: earliest year to consider
-#     :param end_year: latest year to consider
-#     :param temp_column_index: index of the column where the temperature data resides.
-#            Index 3 is max temp,
-#            index 5 is min temp,
-#            index 8 is mean temp for the month.
-#     :return: array of 12 values with the
-#     averages of column temp_column_index for each month.
-#     """
-#
-#     # create some lists of zeros.
-#     month_avg = [0.0] * 12  # running sum of yearly values for each month
-#     counts = [0] * 12  # running count of values added to each sum
-#
-#     for row in temperature_data: # row is sublist
-#         row_year = row[1]
-#         row_month = row[2]
-#         # if the data is not missing, and is within the right year range
-#         if row[temp_column_index + 1] != 'M' and row_year >= start_year and row_year <= end_year:
-#             # accumulate it in the running sum for that month
-#             # Note: record[1] is the year, record[2] is the month (as an integer).
-#             month_avg[row_month - 1] = month_avg[row_month - 1] + row[temp_column_index]
-#             counts[row_month - 1] = counts[row_month - 1] + 1
-#
-#     # compute the averages by dividing each month's running sum by the number of data points.
-#     # we have to use a different count for each month because of missing data items.
-#     for i in range(12):
-#         month_avg[i] = month_avg[i] / counts[i]
-#
-#     return month_avg
+def avg_monthly_temp(temperature_data, start_year,
+                     end_year, temp_column_index):
+    """
+    Calculate the mean of the temperatures in column temp_column_index for the given range of years.
+
+    :param temperature_data: a list in which each item is a temperature data record as a list
+    :param start_year: earliest year to consider
+    :param end_year: latest year to consider
+    :param temp_column_index: index of the column where the temperature data resides.
+           Index 3 is max temp,
+           index 5 is min temp,
+           index 8 is mean temp for the month.
+    :return: array of 12 values with the
+    averages of column temp_column_index for each month.
+    """
+
+    # create some lists of zeros.
+    month_avg = [0.0] * 12  # running sum of yearly values for each month
+    counts = [0] * 12  # running count of values added to each sum
+
+    for row in temperature_data: # row is sublist
+        row_year = row[1]
+        row_month = row[2]
+        # if the data is not missing, and is within the right year range
+        if row[temp_column_index + 1] != 'M' and row_year >= start_year and row_year <= end_year:
+            # accumulate it in the running sum for that month
+            # Note: record[1] is the year, record[2] is the month (as an integer).
+            month_avg[row_month - 1] = month_avg[row_month - 1] + row[temp_column_index]
+            counts[row_month - 1] = counts[row_month - 1] + 1
+
+    # compute the averages by dividing each month's running sum by the number of data points.
+    # we have to use a different count for each month because of missing data items.
+    for i in range(12):
+        month_avg[i] = month_avg[i] / counts[i]
+
+    return month_avg
 
 
 ####### The Main Program #######
